fb3e95245a660c0f86c679ae1d439a34732478c2
Karine <do.karine@gmail.com>
2017-07-11 14:17:21 -0400

    Setup gruntfile and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+module.exports = function(grunt) {
+  'use strict'
+
+  require('load-grunt-tasks')(grunt)
+  require('time-grunt')(grunt)
+
+  // project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      common: {
+        dest: 'common/common.scss',
+        src: '<%= sass.common.dest %>'
+      },
+      desktop: {
+        dest: 'desktop/desktop.scss',
+        src: '<%= sass.desktop.dest %>'
+      },
+      mobile: {
+        dest: 'mobile/mobile.scss',
+        src: '<%= sass.mobile.dest %>'
+      }
+    },
+
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('autoprefixer')({
+            browsers: [
+              'Android 2.3',
+              'Android >= 4',
+              'Chrome >= 35',
+              'Edge >= 12',
+              'Explorer >= 10',
+              'Firefox >= 38',
+              'iOS >= 8',
+              'Opera >= 12',
+              'Safari >= 8'
+            ]
+          })
+        ]
+      },
+      common: {
+        src: 'common/common.scss'
+      },
+      desktop: {
+        src: 'desktop/desktop.scss'
+      },
+      mobile: {
+        src: 'mobile/mobile.scss'
+      }
+    },
+
+    sass: {
+      options: {
+        precision: 3,
+        sourcemap: 'none',
+        style: 'expanded'
+      },
+      common: {
+        dest: 'common/common.scss',
+        src: 'assets/stylesheets/common/common.scss'
+      },
+      desktop: {
+        dest: 'desktop/desktop.scss',
+        src: 'assets/stylesheets/desktop/desktop.scss'
+      },
+      mobile: {
+        dest: 'mobile/mobile.scss',
+        src: 'assets/stylesheets/mobile/mobile.scss'
+      }
+    },
+
+    // update package.json packages
+    devUpdate: {
+      default: {
+        options: {
+          semver: false,
+          updateType: 'prompt'
+        }
+      }
+    }
+  })
+
+  // task registration
+  grunt.registerTask(
+    'default',
+    [
+      'sass',
+      'concat',
+      'postcss'
+    ]
+  )
+}
