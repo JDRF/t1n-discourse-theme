7536ff9d5a85884b6a912f2a52b3f79b2ec69033
Pawan Gupta <pawan.gupta@beyondkey.com>
2024-03-21 17:11:45 +0530

    Revert "Deploy 21 mar 2024"

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
     sass: {
       options: {
         precision: 3,
-        sourcemap: false,
+        sourcemap: 'none',
         style: 'expanded'
       },
       common: {
